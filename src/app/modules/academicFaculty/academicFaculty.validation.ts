f1becf236d4e3a18defb1c3380c6f198e3dcd8c1
Alok Roy <alokroy602701@gmail.com>
2025-11-07 14:10:14 +0600

    complete offered course module

--- src/app/modules/academicFaculty/academicFaculty.validation.ts
+++ src/app/modules/academicFaculty/academicFaculty.validation.ts
@@ -1,12 +1,14 @@
 import { z } from 'zod';
 
 const createAcademicFacultyZodValidation = z.object({
-  name: z
-    .string()
-    .nonempty('Faculty name is required')
-    .trim()
-    .min(2, 'Faculty name must be at least 2 characters long')
-    .max(50, 'Faculty name cannot exceed 50 characters'),
+   body: z.object({
+    name: z
+      .string()
+      .nonempty('Faculty name is required')
+      .trim()
+      .min(2, 'Faculty name must be at least 2 characters long')
+      .max(50, 'Faculty name cannot exceed 50 characters'),
+  }),
 });
 
 const updateAcademicFacultyZodValidation = z.object({
